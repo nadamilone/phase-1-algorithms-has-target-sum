35b5210f1e2610aa890096a61dd3668e592cbc2a
NADA MILONE <nadamilone@gmail.com>
2022-05-31 16:27:07 -0400

    commit tree ec454f211752761844338bcd71b50064f732e22c

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const match = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === match) return true;
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+Add your pseudocode here
+// The function should return true
+//if any pair of numbers in the array
+//adds up to the target number
 */
 
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
+//hasTargetSum([3, 8, 12, 4, 11, 7], 10); //true
+// hasTargetSum([22, 19, 4, 6, 30], 25); // true
+// hasTargetSum([1, 2, 5], 4); //false
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +41,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([10, 12, 5], 11));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([13, 11, 15, 17, 19], 24));
 }
 
 module.exports = hasTargetSum;
